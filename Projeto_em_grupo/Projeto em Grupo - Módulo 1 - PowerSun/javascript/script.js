e94f48a86a816f4e57fc6d76450e5159982411b8
David Lopes Gomes <gomes.david.9@gmail.com>
2022-09-28 19:59:09 -0300

    Delete Projeto_em_grupo directory

--- Projeto_em_grupo/Projeto em Grupo - Módulo 1 - PowerSun/javascript/script.js
+++
@@ -1,5 +0,0 @@
-function exibirAlerta(){
-    let temas=["Eu sou o SOL!", "Vem pro sol", "SoftSkill é inútil!"];
-    var exibicao=temas[(Math.floor(Math.random() * temas.length))];
-    alert(exibicao);
-}
