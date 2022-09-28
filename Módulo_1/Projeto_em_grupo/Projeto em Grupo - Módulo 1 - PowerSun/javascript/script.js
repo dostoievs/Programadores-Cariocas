434c39381529cc86f33793ce383079607097df47
David Lopes Gomes <gomes.david.9@gmail.com>
2022-09-28 20:00:24 -0300

    Utilização de functions e exibição em alert

--- Módulo_1/Projeto_em_grupo/Projeto em Grupo - Módulo 1 - PowerSun/javascript/script.js
+++ Módulo_1/Projeto_em_grupo/Projeto em Grupo - Módulo 1 - PowerSun/javascript/script.js
@@ -1,3 +1,5 @@
-let temas= ["Eu sou o SOL!", "Vem pro sol"];
-var exibicao=temas[(Math.floor(Math.random() * 2))];
-alert(exibicao);
+function exibirAlerta(){
+    let temas=["Eu sou o SOL!", "Vem pro sol", "SoftSkill é inútil!"];
+    var exibicao=temas[(Math.floor(Math.random() * temas.length))];
+    alert(exibicao);
+}
