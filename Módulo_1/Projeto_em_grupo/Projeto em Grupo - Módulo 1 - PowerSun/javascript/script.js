bd068d511e57336eb63b622bf9acb6d765e37054
David Lopes Gomes <gomes.david.9@gmail.com>
2022-10-06 18:23:51 -0300

    Update script.js

--- Módulo_1/Projeto_em_grupo/Projeto em Grupo - Módulo 1 - PowerSun/javascript/script.js
+++ Módulo_1/Projeto_em_grupo/Projeto em Grupo - Módulo 1 - PowerSun/javascript/script.js
@@ -1,5 +1,5 @@
 function exibirAlerta(){
-    let temas=["Eu sou o SOL!", "Vem pro sol", "SoftSkill é inútil!"];
+    let temas=["Eu sou o SOL!", "Vem pro sol", "Sol é energia"];
     var exibicao=temas[(Math.floor(Math.random() * temas.length))];
     alert(exibicao);
 }
