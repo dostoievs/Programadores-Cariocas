e983dd63ae0dfa541cabc2190a9a35e579d7e0d4
David Lopes Gomes <gomes.david.9@gmail.com>
2022-10-19 21:34:53 -0300

    Conversor base64 implementado

--- Módulo_2/Projeto_Individual - CodificadorDecodificador/javascript/script.js
+++ Módulo_2/Projeto_Individual - CodificadorDecodificador/javascript/script.js
@@ -1,6 +1,18 @@
-function myFunction(teste){
-   alert(btoa(teste));
+function myFunction(){
+   const base64='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789/=';
+   tamanhoBase64=base64.length;
+   var select = document.getElementById('acao_realizada');
+	var acao = select.options[select.selectedIndex].value;
+   select = document.getElementById('algoritmo');
+	var algoritmo = select.options[select.selectedIndex].value;
+   var codificar = document.getElementById('codificar').value;
+   if((acao=='codificar')&&(algoritmo=='base64')){
+      code =window.btoa(codificar);
+      alert(code);
+   }
+   if((acao=='decodificar')&&(algoritmo=='base64')){  
+      encode=window.atob(codificar);
+      alert(encode);
+   }
    return 0;
 }
-// conversão string para base64
-    //atob()// conversão base64 para string
