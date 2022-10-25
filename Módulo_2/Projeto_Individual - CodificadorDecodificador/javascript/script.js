2497b439e084c7ac559b52f80658d9ab3e233791
David Lopes Gomes <gomes.david.9@gmail.com>
2022-10-25 07:16:35 -0300

    Implementação botão radio

--- Módulo_2/Projeto_Individual - CodificadorDecodificador/javascript/script.js
+++ Módulo_2/Projeto_Individual - CodificadorDecodificador/javascript/script.js
@@ -1,4 +1,11 @@
-function mostra_campo_deslocamento(obj){
+function mudar_texto_botao(){
+   console.log("teste")
+   $("#submit").click(function(){
+      $(this).text("Teste");
+    });
+}
+
+function mostra_campo_deslocamento(){
    var select = document.getElementById('algoritmo');
    var input_deslocamento = document.getElementById('deslocamento');
    input_deslocamento.style.visibility = (select.value == 'cifra_de_cesar') 
@@ -10,8 +17,15 @@ function codificar(){
    var alfabeto_maiusculo = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
    var tamanho_alfabeto_maiusculo=alfabeto_maiusculo.length;
    var tamanho_alfabeto_minusculo=alfabeto_minusculo.length;
-   var select = document.getElementById('acao_realizada');
-	var acao = select.options[select.selectedIndex].value;
+   var acao;
+   if (document.getElementById("acao_realizada_codificar").checked) {
+      acao=document.getElementById("acao_realizada_codificar").value;
+      console.log(acao);
+   } 
+   else if (document.getElementById("acao_realizada_decodificar").checked) {
+      acao=document.getElementById("acao_realizada_decodificar").value;
+      console.log(acao);
+   }
    select = document.getElementById('algoritmo');
 	var algoritmo = select.options[select.selectedIndex].value;
    var codificar = document.getElementById('codificar').value;
@@ -25,7 +39,7 @@ function codificar(){
    }
    if((acao=='codificar')&&(algoritmo=='cifra_de_cesar')){
       var deslocamento=document.getElementById('input_deslocamento').value;
-      if((deslocamento=='')||(isNaN(deslocamento))){
+      if((deslocamento=='')||(isNaN(deslocamento))||(deslocamento<0)||(deslocamento>tamanho_alfabeto_maiusculo)){
          alert('O deslocamento precisa ser um número inteiro válido');
       }
       var tamanhoCodificar=codificar.length;
@@ -49,8 +63,8 @@ function codificar(){
    }
    if((acao=='decodificar')&&(algoritmo=='cifra_de_cesar')){
       var deslocamento=document.getElementById('input_deslocamento').value;
-      if((deslocamento=='')||(isNaN(deslocamento))){
-         alert('O deslocamento precisa ser um número inteiro válido');
+      if((deslocamento=='')||(isNaN(deslocamento))||(deslocamento<0)||(deslocamento>tamanho_alfabeto_maiusculo)){
+         alert(`O deslocamento precisa ser um número inteiro válido entre 0 e ${tamanho_alfabeto_maiusculo}`);
       }
       var tamanhoCodificar=codificar.length;
       var posicao_maiusculo;
