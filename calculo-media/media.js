05800e9527087270f6b45dd2a9db4438af23df0f
David Lopes Gomes <gomes.david.9@gmail.com>
2022-11-25 21:34:32 -0300

    Add files via upload

--- calculo-media/media.js
+++ calculo-media/media.js
@@ -1,6 +1,6 @@
 
 const evento=document.querySelector('form');
-const h1 = document.querySelector('h1');
+const resultado = document.getElementById('resultado');
 evento.addEventListener('submit', (e)=>{
     const nome= evento.nome.value;
     const nota1 = Number(evento.nota1.value);
@@ -8,6 +8,6 @@ evento.addEventListener('submit', (e)=>{
     const nota3 = Number(evento.nota3.value);
     const nota4 = Number(evento.nota4.value);
     let media =(nota1+nota2+nota3+nota4)/4;
-    h1.innerText=`O aluno ${nome} tem nota ${media}`;
+    resultado.value=`${media}`;
     e.preventDefault();
 })
