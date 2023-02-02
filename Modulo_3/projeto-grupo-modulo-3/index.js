ffca3663545df7a0d3c655f9f57b8e634f71d1ee
David Lopes Gomes <gomes.david.9@gmail.com>
2023-02-02 19:01:32 -0300

    Update index.js

--- Modulo_3/projeto-grupo-modulo-3/index.js
+++ Modulo_3/projeto-grupo-modulo-3/index.js
@@ -7,7 +7,7 @@ const PORT = 3000;
 
 const jsonServer = require('json-server')
 const server = jsonServer.create()
-const router = jsonServer.router('.server/mock/db.json')
+const router = jsonServer.router('./server/mock/db.json')
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
