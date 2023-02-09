49677b87e820c705a6d5c1b7c383fd6dc583ba71
David Lopes Gomes <gomes.david.9@gmail.com>
2023-02-09 20:54:31 -0300

    Update Contato.jsx

--- Modulo_3/projeto-grupo-modulo-3/src/components/Contato.jsx
+++ Modulo_3/projeto-grupo-modulo-3/src/components/Contato.jsx
@@ -3,21 +3,30 @@ function Contato() {
         <form>
             <div className="form-group">
                 <label htmlFor="formGroupExampleInput">Nome:</label>
-                <input type="text" className="form-control" id="formGroupExampleInput" placeholder="Example input" />
+                <input type="text" className="form-control" id="formGroupExampleInput" placeholder="Digite seu Nome" required/>
+                <div className="invalid-feedback">
+                    Digite seu Nome
+                </div>
             </div>
             <div className="form-group">
                 <label htmlFor="formGroupExampleInput2">Sobrenome:</label>
-                <input type="text" className="form-control" id="formGroupExampleInput2" placeholder="Another input" />
+                <input type="text" className="form-control" id="formGroupExampleInput2" placeholder="Digite seu Sobrenome" required/>
+                <div className="invalid-feedback">
+                    Digite seu Sobrenome
+                </div>
             </div>
             <div className="form-group">
                 <label htmlFor="formGroupExampleInput2">E-mail:</label>
-                <input type="text" className="form-control" id="formGroupExampleInput2" placeholder="Another input" />
+                <input type="email" className="form-control" id="formGroupExampleInput2" placeholder="Digite seu E-mail" required/>
+                <div className="invalid-feedback">
+                    Digite um e-mail valido
+                </div>
             </div>
-            <div class="mb-3">
-                <label for="validationTextarea">Mensagem:</label>
-                <textarea class="form-control is-invalid" id="validationTextarea" placeholder="Required example textarea" required></textarea>
-                <div class="invalid-feedback">
-                    Please enter a message in the textarea.
+            <div className="mb-3">
+                <label htmlFor="validationTextarea">Mensagem:</label>
+                <textarea className="form-control is-invalid" id="validationTextarea" placeholder="Digite sua Mensagem" required></textarea>
+                <div className="invalid-feedback">
+                    Escreva sua Mensagem
                 </div>
             </div>
         </form>
