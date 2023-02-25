c918b54bd40fb401479e437a8cd41a16bd76d394
David Lopes Gomes <gomes.david.9@gmail.com>
2023-02-25 14:19:05 -0300

    Update Comanda.jsx

--- Modulo_3/spaceBurguer/grupo3/src/routes/Comanda.jsx
+++ Modulo_3/spaceBurguer/grupo3/src/routes/Comanda.jsx
@@ -12,7 +12,7 @@ function Comanda() {
   }, [])
   async function loadComanda() {
     try {
-      const resp = await fetch(`https://projeto-individual-3-uy0v.onrender.com/comanda`, {
+      const resp = await fetch(`https://server-space-burguer.onrender.com/comanda`, {
         method: 'GET',
       });
       console.log('resposta fetch->', resp)
